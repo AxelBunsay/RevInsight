acdb19fd54d041a6ad838910f46954ca3a18b4a4
AxelBunsay <akurunogia12@gmail.com>
2026-04-24 17:25:01 +0800

    wompwomp

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('BADING');
+app.get('/', (req, res) => {
+  res.json('Womp womp');
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 })
